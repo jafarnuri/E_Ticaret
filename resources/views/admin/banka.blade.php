675d1678bb61f8fa047df62602e9297daf5faa55
jafarnuri <jafarnuri1994@gmail.com>
2023-10-22 19:43:55 +0400

    Update banka.blade.php

--- resources/views/admin/banka.blade.php
+++ resources/views/admin/banka.blade.php
@@ -1,12 +1,12 @@
 @extends('admin_layout.master')
 
-@section('content')
+@section('contect')
 <!-- page content -->
 <div class="right_col" role="main">
   <div class="">
 
     <div class="clearfix"></div>
-    <div class="row">
+     <div class="row">
       <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
           <div class="x_title">
